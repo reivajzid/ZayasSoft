a37966117de2b6f2c27f13939c69f2a3ad68b4b5
reivaj <javierdizsantos@gmail.com>
2015-04-12 16:46:08 +0200

    Version 1.1

--- ZayasSoft/src/MODULO1.java
+++ ZayasSoft/src/MODULO1.java
@@ -1,4 +1,19 @@
+import java.util.Scanner;
 
 public class MODULO1 {
-  
+    public static void main(String[] args) {
+        
+    	int num;
+        System.out.print("Introduzca un numero: ");
+        Scanner sc = new Scanner(System.in);
+        num= sc.nextInt();
+        System.out.println("---------------------------");
+        mostrar(num);
+        System.out.println("---------------------------");
+    }
+    static void mostrar(int num){
+        for (int i=0;i<num;i++){
+            System.out.println("Módulo ejecutándose");
+        }
+    }
 }
